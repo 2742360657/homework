a87b69fa02b1e2b872211ee0c3389d1d4ec9fe43
susnowy <2742360657@qq.com>
2025-11-21 10:44:56 +0800

    载入实验三

---
+++ experiment3/string_array.cpp
@@ -0,0 +1,29 @@
+#include "string_array.h"
+
+using namespace std;
+
+void myStr::print(){
+    for(int i = 0; i < this->length; i++)
+        cout << this->data[i];
+
+    cout << endl;
+}
+
+void myStr::printNext(){
+    for(int i = 0; i < length; i++)
+        cout << next[i];
+    cout << endl;
+}
+
+void myStr::printNextVal(){
+    for(int i = 0; i < length; i++)
+        cout << nextVal[i];
+    cout << endl;
+}
+
+void myMatrix::printMatrix(){
+    cout << rowNum << "," << colNum << "," << nodeNum << endl;
+
+    for(int i = 0; i < nodeNum; i++)
+        cout << nodeList[i].row << "," << nodeList[i].col << "," << nodeList[i].value << endl;
+}
