fd9b097f4e67f9584148032a7724540a0554be61
susnowy <2742360657@qq.com>
2025-11-21 15:50:30 +0800

    myStr的构造和析构完成

--- experiment3/string_array.cpp
+++ experiment3/string_array.cpp
@@ -16,36 +16,79 @@ static void buildNextVal(const char* pat, int m, int* nextVal1)
 //myStr实现-------------------------------------
 
 myStr::myStr()
+    :length(0),next(nullptr),nextVal(nullptr),data(new char[1])
 {
-
+    data[0]='\0';
 }
 
 myStr::myStr(const int& len)
+    :length(len),next(nullptr),nextVal(nullptr)
 {
-
+    if(len<=0)
+    {
+        std::cout<<"非法长度,设置为空串\n";
+        length=0;
+        data=new char[1];
+        data[0]='\0';
+        return;
+    }
+    data=new char[len+1];
+    for(int i=0;i<len;i++)
+    {
+        data[i]='0';
+    }
+    data[len]='\0';
 }
 
 myStr::myStr(const char* s)
+    :next(nullptr),nextVal(nullptr)
 {
-
+    if(!s)
+    {
+        std::cout<<"违规字符串,设置为空串\n";
+        length=0;
+        data=new char[1];
+        data[0]='\0';
+        return;
+    }
+    length=std::strlen(s);
+    data=new char[length+1];
+    for(int i=0;i<=length;i++)
+    {
+        data[i]=s[i];
+    }
+    return;
 }
 
 myStr::myStr(const myStr& other)
+    :length(other.length),next(nullptr),nextVal(nullptr)
 {
-
+    if (other.data == nullptr)
+    {
+        std::cout<<"无data,设置为空串\n";
+        data=new char[1];
+        data[0]='\0';
+        return;
+    }
+    data=new char[length+1];
+    for(int i=0;i<=length;i++)
+    {
+        data[i]=other.data[i];
+    }
+    return;
 }
 
 myStr::~myStr()
 {
-
+    delete[] data;
 }
 
 void myStr::print()
 {
     for(int i = 0; i < this->length; i++)
-        cout << this->data[i];
+        std::cout << this->data[i];
 
-    cout << endl;
+    std::cout << std::endl;
 }
 
 void myStr::kmp_next()
@@ -60,14 +103,14 @@ void myStr::kmp_nextVal()
 
 void myStr::printNext(){
     for(int i = 0; i < length; i++)
-        cout << next[i];
-    cout << endl;
+        std::cout << next[i];
+    std::cout << std::endl;
 }
 
 void myStr::printNextVal(){
     for(int i = 0; i < length; i++)
-        cout << nextVal[i];
-    cout << endl;
+        std::cout << nextVal[i];
+    std::cout << std::endl;
 }
 
 //友元函数-----------------------------------------
@@ -113,10 +156,10 @@ myMatrix::~myMatrix()
 
 void myMatrix::printMatrix()
 {
-    cout << rowNum << "," << colNum << "," << nodeNum << endl;
+    std::cout << rowNum << "," << colNum << "," << nodeNum << std::endl;
 
     for(int i = 0; i < nodeNum; i++)
-        cout << nodeList[i].row << "," << nodeList[i].col << "," << nodeList[i].value << endl;
+        std::cout << nodeList[i].row << "," << nodeList[i].col << "," << nodeList[i].value << std::endl;
 }
 
 void myMatrix::FastTransposeSMatrix(myMatrix& T)
